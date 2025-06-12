d7992ef38e3c7bde4023d1ecb1c85073830dd410
Huang-xiaoyi <1179520796@qq.com>
2025-06-12 10:53:43 +0800

    Kaggle Lesson 1 exercise 2

---
+++ wash/fruit2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Your code goes here. Create a dataframe matching the above diagram and assign it to the variable fruits.
+fruit_sales = pd.DataFrame({
+    'Apples': [35, 41],
+    'Bananas': [21, 34]
+}, index=['2017 Sales', '2018 Sales'])
+# Check your answer
+# q1.check()
+input(fruit_sales)
